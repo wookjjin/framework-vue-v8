60b3e2539a64df9012f1765ac67243e6ab7933ce
jinwook <57286129+wookjjin@users.noreply.github.com>
2026-03-10 09:34:45 +0900

    Enhance ESLint config with file-specific rules
    Added specific rules for .vue and .ts files in ESLint configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,23 @@
 import antfu from '@antfu/eslint-config'
 
-export default antfu({
-  vue: true,
-})
+export default antfu(
+  {
+    vue: true,
+  },
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'no-console': 'off',
+      'vue/operator-linebreak': ['error', 'before'],
+      'antfuw/top-level-function': 'off',
+    },
+  },
+  {
+    files: ['**/*.ts'],
+    rules: {
+      'node/prefer-global/process': 'off',
+      'ts/consistent-type-definitions': 'off',
+    },
+  },
+
+)
